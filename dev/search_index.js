e39aefdf2abf6c78fb81d7e8db0e74310e5438b6
Documenter.jl <documenter@juliadocs.github.io>
2023-02-08 14:41:43 +0000

    build based on 67b8835

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GFTools","category":"page"},{"location":"#GFTools","page":"Home","title":"GFTools","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GFTools.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GFTools]","category":"page"}]
+}
